93f3246f129d03a946a58e1ce7700e48fd659196
Dave Cohen <dave@bookbub.com>
2023-09-28 12:04:25 -0400

    Initial Commit

---
+++ lovebrarian/lovebrarianApp.swift
@@ -0,0 +1,17 @@
+//
+//  lovebrarianApp.swift
+//  lovebrarian
+//
+//  Created by Dave Cohen on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct lovebrarianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
